04e439bb78cc9dd768b96642ebd00d7d81e324ca
ugokingsley <ugokingsley5@gmail.com>
2022-09-22 22:01:36 +0100

    finished post and read request

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BookItems/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookItems from "./components/BookItems.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BookItems,
   }
 }
 </script>
---
+++ src/components/BookItems.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="books_container">
+        <div class="books_content">
+            <h1>Books</h1>
+            <h1>Tasks</h1>
+            <ul class="tasks_list">
+                <li v-for="book in books" :key="book.id">
+                    <h2>{{ book.name }}</h2>
+                    <p>{{ book.isbn }}</p>
+                    <p>{{ book.author }}</p>
+        
+                    <button @click="updateBook(book)">Edit</button>
+                </li>
+            </ul>
+        </div>
+        <div class="add_book">
+          <form v-on:submit.prevent="submitForm">
+            <div class="form-group">
+              <label for="name">Book Name</label>
+              <input type="text" class="form-control" id="name" v-model="name" />
+            </div>
+            <div class="form-group">
+              <label for="isbn">ISBN</label>
+              <input type="text" class="form-control" id="isbn" v-model="isbn" />
+            </div>
+            <div class="form-group">
+              <label for="author">Author</label>
+              <input type="text" class="form-control" id="author" v-model="author" />
+            </div>
+            
+            <div class="form-group">
+              <button type="submit">Add Book</button>
+            </div>
+          </form>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                books: [],
+                name: "",
+                isbn: "",
+                author:"",
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    const response = await axios.get('http://localhost:5000/api/books');
+                    this.books = response.data; 
+                    console.log(this.books);
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+
+            async submitForm() {
+              try {
+                const response = await axios.post('http://localhost:5000/api/books', {
+                    name: this.name,
+                    isbn: this.isbn,
+                    author: this.author,
+                  }
+                );
+                this.books.push(response.data);
+                this.name = "";
+                this.isbn = "";
+                this.author = "";
+              } catch (error) {
+                console.log(error);
+              }
+            },
+
+            async updateBook(book){
+                try{
+                const response = await axios.put('http://localhost:5000/api/books/${book.id}', {
+                        name: book.name,
+                        isbn: this.isbn,
+                        author: this.author,
+                    });
+                    let bookIndex = this.books.findIndex(t => t.id === book.id);
+                    this.books = this.books.map((book) => {
+                        if(this.books.findIndex(t => t.id === book.id) === bookIndex){
+                            return response.data;
+                        }
+                        return book;
+                    });
+
+                }catch(error){
+                    console.log(error);
+                }
+            }
+        },
+        created() {
+            this.getData();
+        }
+    }
+</script>
